f1ebdb4c25e371cc4bf430bb7e7e5ed9211fd635
Marcielle Janguas <marciellejanguas@gmail.com>
2025-05-20 16:37:38 -0300

    criação das funções

--- src/js/script.js
+++ src/js/script.js
@@ -26,35 +26,17 @@ function classificarVinho(safra) {
     return "Antigo";
 }
 
-const vinhos = [{nome: "Gabriel", tipo: "Rosé", safra: "2022", estoque:10"}]
+function adicionarVinho(nome, tipo, safra, estoque) {
+  vinhos.push({ nome, tipo, safra, estoque });
+}
 
-    while (continuar && contador < 10) {
-        contador++;
+const vinhos = [{nome: "Gabriel", tipo: "Rosé", safra: "2022", estoque:"10"}]
 
-        nomeVinho = validarEntrada("Digite o nome do vinho:");
-        tipoVinho = validarEntrada("Digite o tipo do vinho:");
-        safraVinho = parseInt(validarEntrada("Digite o ano da safra:"));
-        let quantidade = parseInt(validarEntrada("Digite a quantidade em estoque:"));
+function iniciar() {
+  adicionarVinho("Gabriel", "rosé", 2020, 3);
 
-        let classificacao = classificarVinho(safraVinho);
-        let alerta = estoqueBaixo(quantidade);
-
-        mostrarDados(nomeVinho, tipoVinho, safraVinho, quantidade, classificacao, alerta);
-
-        if (alerta) estoqueBaixoTotal++;
-        if (safraVinho < safraMaisAntiga) {
-            safraMaisAntiga = safraVinho;
-            nomeSafraMaisAntiga = nomeVinho;
-        }
-
-        continuar = confirm("Deseja cadastrar outro vinho?");
-    }
-
-    alert("Cadastro finalizado!\n" +
-        "Total de vinhos cadastrados: " + contador + "\n" +
-        "Vinhos com estoque baixo: " + estoqueBaixoTotal + "\n" +
-        "Safra mais antiga: " + safraMaisAntiga + " (" + nomeSafraMaisAntiga + ")" + "\n" +
-        "Veja as informações no console.");
+  console.log("Lista completa de vinhos:");
+  vinhos.forEach(v => mostrarDados(v));
 }
 
 iniciar();
