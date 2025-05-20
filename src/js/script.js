8bbeb2605eb76a50e47eb59d52c1304fc13ab0ed
Marcielle Janguas <marciellejanguas@gmail.com>
2025-05-20 17:38:40 -0300

    organizações das funções reutilizáveis e inclusão de comentários

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,12 @@ function mostrarDados(vinho) {
     console.log("Quantidade: " + vinho.estoque);
 }
 
+//função para verificar a quantidade de vinhos em estoque
 function estoqueBaixo(quantidade) {
     return quantidade < 5;
 }
 
+//funções reutilizáveis
 function adicionarVinho(nome, tipo, safra, estoque) {
     let vinho = {};
     vinho.nome = nome;
@@ -19,23 +21,51 @@ function adicionarVinho(nome, tipo, safra, estoque) {
     vinhos.push(vinho);
 }
 
-const vinhos = []
-
-function iniciar() {
-    adicionarVinho("Gabriel", "rosé", "2020", 28);
-
+function listarVinhos() {
     console.log("Lista completa de vinhos:");
     vinhos.forEach(vinho => mostrarDados(vinho));
+}
 
+function verificaEstoqueBaixo() {
     const baixos = vinhos.filter(vinho => estoqueBaixo(vinho.estoque));
     console.log("\n Estoque abaixo de 5:");
     baixos.forEach(v => console.log(`${v.nome} - Apenas ${v.estoque} unidades`));
+}
 
+function verificaTotalEstoque() {
     const totalEstoque = vinhos.reduce((soma, vinho) => soma + vinho.estoque, 0);
     console.log("\nEstoque total: " + totalEstoque + " garrafas");
+}
 
+function nomesCaixaAlta() {
     const nomesCaixaAlta = vinhos.map(vinho => vinho.nome.toUpperCase());
     alert(" Vinhos:\n" + nomesCaixaAlta.join("\n"));
+}
+
+// array de vinhos cadastrados 
+const vinhos = [
+    { nome: "Matheus", tipo: "rosé", safra: 2020, estoque: 28 },
+    { nome: "Cabernet", tipo: "tinto", safra: 2018, estoque: 3 },
+    { nome: "Chardonnay", tipo: "branco", safra: 2019, estoque: 10 },
+    { nome: "Malbec", tipo: "tinto", safra: 2017, estoque: 2 },
+    { nome: "Pinot Noir", tipo: "tinto", safra: 2021, estoque: 15 }
+]
+
+function iniciar() {
+    // simula cadastro de vinho
+    adicionarVinho("Matheus", "rosé", "2020", 28);
+
+    //mostrar lista dos vinhos
+    listarVinhos()
+
+    // verifica estoque
+    verificaEstoqueBaixo()
+
+    // mostrar total em estoque
+    verificaTotalEstoque()
+
+    // mostrar lista de vinhos em caixa alta via ALERT
+    nomesCaixaAlta()
 }
 
 iniciar();
