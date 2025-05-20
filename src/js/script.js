1eb735203ceb5de90f3af2536815413513553d9b
Marcielle Janguas <marciellejanguas@gmail.com>
2025-05-20 16:43:02 -0300

    exclusão da classificação e inclusão da verificação de estoque

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,14 @@
-function mostrarDados(nome, tipo, safra, quantidade, classificacao, alerta) {
+function mostrarDados(nome, tipo, safra, quantidade, alerta) {
     console.log("---------DETALHES----------");
     console.log("Nome: " + nome);
     console.log("Tipo: " + tipo);
     console.log("Safra: " + safra);
     console.log("Quantidade: " + quantidade);
-    console.log("Classificação: " + classificacao);
     if (alerta) alert("Estoque baixo para: " + nome);
-}
-
-function validarEntrada(mensagem) {
-    let entrada;
-    do {
-        entrada = prompt(mensagem);
-    } while (entrada === null || entrada.trim() === "");
-    return entrada;
 }
 
 function estoqueBaixo(quantidade) {
     return quantidade < 5;
-}
-
-function classificarVinho(safra) {
-    if (safra >= 2020) return "Jovem";
-    if (safra >= 2015) return "Amadurecido";
-    return "Antigo";
 }
 
 function adicionarVinho(nome, tipo, safra, estoque) {
@@ -33,10 +18,15 @@ function adicionarVinho(nome, tipo, safra, estoque) {
 const vinhos = [{nome: "Gabriel", tipo: "Rosé", safra: "2022", estoque:"10"}]
 
 function iniciar() {
-  adicionarVinho("Gabriel", "rosé", 2020, 3);
+  adicionarVinho("Gabriel", "rosé", 2020, 27);
 
   console.log("Lista completa de vinhos:");
   vinhos.forEach(v => mostrarDados(v));
+  
+  const baixos = vinhos.filter(v => estoqueBaixo(v.estoque));
+  console.log("\n Estoque abaixo de 5:");
+  baixos.forEach(v => console.log(`${v.nome} - Apenas ${v.estoque} unidades`));
+
 }
 
 iniciar();
